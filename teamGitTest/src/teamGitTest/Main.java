5416ddee7891b00edcf6a5423e48a5a1baec5c9b
chahyoil <chahyoil@gmail.com>
2024-09-13 11:15:26 +0900

    [add] ay작성

--- teamGitTest/src/teamGitTest/Main.java
+++ teamGitTest/src/teamGitTest/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bye World");
+		System.out.println("title 정해주세요");
 
 	}
 		// HKS 수정
